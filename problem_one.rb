05bf81ecbc450a2ad41ce9ba688f36e4df7db5eb
James Bond <jamesbond.home@gmail.com>
2012-12-14 08:33:32 -0800

    make it a String method

--- problem_one.rb
+++ problem_one.rb
@@ -1,15 +1,16 @@
-def simtot(string)
-  total=0
-  length = string.length
-  (0..length-1).each do |pos|
-    total = total + sim(string, string[pos, length-pos])
+class String
+  def total_simularity
+    total = 0
+    (0..length-1).each do |pos|
+      total = total + simularity(self[pos, length-pos])
+    end
+    total
   end
-  total
-end
 
-def sim(string, suffix)
-  for i in 0..suffix.length
-    break if suffix[i] != string[i]
+  def simularity(suffix)
+    for i in 0..suffix.length
+      break if suffix[i] != self[i]
+    end
+    i
   end
-  i
 end
